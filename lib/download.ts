32e2ce341bec31d567d7b5947969043023b57dcd
Marcos Rippel <rippel.marcos@gmail.com>
2025-06-29 11:41:00 -0300

    Trabalho de 2025-06-29 (parte 23) - 2 arquivos

---
+++ lib/download.ts
@@ -0,0 +1,11 @@
+export function download(data: Uint8Array, filename: string, type: string) {
+  const blob = new Blob([data], { type });
+  const url = window.URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+  window.URL.revokeObjectURL(url);
+  document.body.removeChild(a);
+}
